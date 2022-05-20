36a925dac6956453dd8c3dd7839518ee6c77abc7
José Antônio Sousa da Silva <tony@arpiatecnologia.com.br>
2022-05-19 22:09:47 -0300

    refactor: path

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,13 +1,14 @@
 import { Avatar, Badge, Button, Modal } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Header from '../../components/Header';
+import Header from '../../Components/Header';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import {
   handleDecrement,
   handleIncrement,
   selectMyNum
 } from '../../slices/myReducer/mySlice';
+
 
 const Dashboard = () => {
   const dispatch = useAppDispatch();
@@ -39,39 +40,39 @@ const Dashboard = () => {
     <>
       <Modal
         title="Testando Redux"
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
+        visible={ isModalVisible }
+        onOk={ handleOk }
+        onCancel={ handleCancel }
       >
         <p>Teste o Redux pressionando os botões desta página</p>
       </Modal>
 
       <Header />
 
-      <div style={{ marginLeft: '2rem' }}>
+      <div style={ { marginLeft: '2rem' } }>
         <h3>Teste o Redux nos botões abaixo!</h3>
-        <span style={{ display: 'flex' }}>
+        <span style={ { display: 'flex' } }>
           <Button
             type="primary"
             danger
-            onClick={() => dispatch(handleDecrement())}
-            style={{ margin: '2rem' }}
+            onClick={ () => dispatch(handleDecrement()) }
+            style={ { margin: '2rem' } }
           >
-            {' '}
-            -{' '}
+            { ' ' }
+            -{ ' ' }
           </Button>
 
-          <Badge count={myNum}>
+          <Badge count={ myNum }>
             <Avatar shape="circle" size="large" />
           </Badge>
 
           <Button
             type="primary"
-            onClick={() => dispatch(handleIncrement())}
-            style={{ margin: '2rem' }}
+            onClick={ () => dispatch(handleIncrement()) }
+            style={ { margin: '2rem' } }
           >
-            {' '}
-            +{' '}
+            { ' ' }
+            +{ ' ' }
           </Button>
         </span>
       </div>
